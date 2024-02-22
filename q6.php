6269c7b236885c1b9978dd188b017bcc45f2b84d
Jaysharma-01 <160105414+Jaysharma-01@users.noreply.github.com>
2024-02-22 13:31:31 +0530

    assignments upload
    web tech assignment

---
+++ q6.php
@@ -0,0 +1,9 @@
+<?php
+$username="jaysharma";
+$password="jay123";
+if ($username=="jaysharma" and $password=="jay123"){
+    echo "LOGIN SUCCESSFUL";
+}else {
+    echo "INVALID CREDENTIALS";
+}
+?>
